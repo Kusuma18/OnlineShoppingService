f17399ef5de9dd90c6aa1a901deb9b526cea3977
Kusuma18 <kusuma.sv@harman.com>
2020-04-19 19:49:07 +0530

    First commit

---
+++ OnlineShoppingService/Controllers/MyCardsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using OnlineShoppingService.Models;
+
+namespace OnlineShoppingService.Controllers
+{
+    public class MyCardsController : ApiController
+    {
+        private MyCardDBContext db = new MyCardDBContext();
+
+        public IQueryable<MyCard> GetCards()
+        {
+            return db.Cards;
+        }
+
+        [ResponseType(typeof(MyCard))]
+        public IHttpActionResult GetMyCard(int id)
+        {
+            MyCard myCard = db.Cards.Find(id);
+            if (myCard == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(myCard);
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMyCard(int id, MyCard myCard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != myCard.CardID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(myCard).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MyCardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [ResponseType(typeof(MyCard))]
+        public IHttpActionResult PostMyCard(MyCard myCard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cards.Add(myCard);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = myCard.CardID }, myCard);
+        }
+
+        [ResponseType(typeof(MyCard))]
+        public IHttpActionResult DeleteMyCard(int id)
+        {
+            MyCard myCard = db.Cards.Find(id);
+            if (myCard == null)
+            {
+                return NotFound();
+            }
+
+            db.Cards.Remove(myCard);
+            db.SaveChanges();
+
+            return Ok(myCard);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MyCardExists(int id)
+        {
+            return db.Cards.Count(e => e.CardID == id) > 0;
+        }
+    }
+}
---
+++ OnlineShoppingService/Models/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShoppingService.Models
+{
+    [Table("MyCards")]
+    public class MyCard
+    {
+        [Key]
+        public int CardID { get; set; }
+        public string CardName { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+    public class MyCardDBContext : DbContext
+    {
+        public DbSet<MyCard> Cards { get; set; }
+    }
+}
